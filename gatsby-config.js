c34f545e09fba0df4b7d89fc8faaec8f129dce55
Atul <atulanand94@gmail.com>
2018-08-07 00:43:00 +0530

    change the site url to atulr.com

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Blog',
     author: 'Atul R',
     description: 'Build. Hack. Rebuild. Hack...',
-    siteUrl: 'https://master-atul.github.io/blog',
+    siteUrl: 'https:/www.atulr.com/blog',
   },
   pathPrefix: '/blog',
   plugins: [
