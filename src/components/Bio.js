5e930188ce9d45f010433f91ac18b6a09031d1c9
Atul <atulanand94@gmail.com>
2019-01-16 00:32:58 +0100

    Fix bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -22,13 +22,43 @@ const Bio = () => (
   <section>
     <p style={styles.intro}>
       <img src={config.image} alt="Atul R" style={styles.avatar} />
-      Written by<strong> <a href="https://www.atulr.com" target="_blank" rel="noopener noreferrer">Atul R</a></strong>{' '}
-      a full-stack web developer 🖥,{' '}<a href="https://www.reactnative.guide" target="_blank" rel="noopener noreferrer">author</a> 📖 and trainer 👨🏽‍🎓.
-      He primarily works on Javascript ecosystem and
-      occasionally hacks around in C++, Rust and Python.
-      He is an  <a href="https://github.com/master-atul" target="_blank" rel="noopener noreferrer">open source</a> enthusiast
-      and <span style={{ color: 'red' }}>❤</span> making useful tools for humans. {' '}
-      <a href="https://twitter.com/masteratul94" target="_blank" rel="noopener noreferrer">You should follow him on Twitter  </a>
+      Written by
+      <strong>
+        {' '}
+        <a
+          href="https://www.atulr.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Atul R
+        </a>
+      </strong>{' '}
+      a developer 🖥,{' '}
+      <a
+        href="https://www.reactnative.guide"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        author
+      </a>{' '}
+      📖 and trainer 👨🏽‍🎓. He primarily works on Javascript ecosystem and
+      occasionally hacks around in C++, Rust and Python. He is an{' '}
+      <a
+        href="https://github.com/master-atul"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        open source
+      </a>{' '}
+      enthusiast and <span style={{ color: 'red' }}>❤</span> making useful tools
+      for humans.{' '}
+      <a
+        href="https://twitter.com/masteratul94"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        You should follow him on Twitter{' '}
+      </a>
     </p>
   </section>
 );
